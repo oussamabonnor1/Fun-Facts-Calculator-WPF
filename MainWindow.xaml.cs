116e4d37879a536c28e4a866d312daf839526825
oussamabonnor1 <oussamabonnor@gmail.com>
2019-03-13 23:12:25 +0100

    first upload,adding files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
